32c9057bf6e06c70dbedbcf32cf9dcd5ff9f1d13
hibiki-sato <82596100+hibiki-sato@users.noreply.github.com>
2021-10-06 13:47:50 +0900

    Add files via upload

---
+++ Thama10/Thama10/ListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ListViewController.swift
+//  Thama10
+//
+//  Created by 佐藤響 on 2021/10/06.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 47
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var identifier = ""
+        if indexPath.row % 3 == 0 {
+            identifier = "Cell1"
+        } else if indexPath.row % 3 == 1 {
+            identifier = "Cell2"
+        } else {
+            identifier = "Cell3"
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = prefectures[indexPath.row]
+        cell.detailTextLabel?.text = "\(indexPath.row + 1)番目の都道府県です"
+        return cell
+    }
+
+    let prefectures = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県",
+                       "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県",
+                       "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県",
+                       "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県",
+                       "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
+                       "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県",
+                       "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県",
+                       "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
+
+}
